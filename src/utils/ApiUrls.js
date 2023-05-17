e2455ccce6c9e2cb835c4357978e7aeb673b8e3a
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-05-17 13:25:14 +0200

    API URLs added

---
+++ src/utils/ApiUrls.js
@@ -0,0 +1,13 @@
+// Urls for the API and it's various endpoints
+
+const BASE_URL = "https://api.noroff.dev/api/v1/holidaze";
+
+const AUTH_URL = `${BASE_URL}/auth`;
+export const LOGIN_URL = `${AUTH_URL}/login`;
+export const REGISTER_URL = `${AUTH_URL}/register`;
+
+export const PROFILES_URL = `${BASE_URL}/profiles`; // Get all profiles
+
+export const VENUES_URL = `${BASE_URL}/venues`;
+
+export const BOOKINGS_URL = `${BASE_URL}/bookings`;
