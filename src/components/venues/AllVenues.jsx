77a15e23bc9f77d8cf5bc46339e36450fb947d83
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-05-20 23:54:17 +0200

    added search functionality to the page with all venues

--- src/components/venues/AllVenues.jsx
+++ src/components/venues/AllVenues.jsx
@@ -12,6 +12,7 @@ async function fetchData() {
 export function VenuesList() {
   const [data, setData] = useState([]);
   const [formattedDates, setFormattedDates] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetchData().then((result) => {
@@ -29,23 +30,36 @@ export function VenuesList() {
     });
   }, []);
 
+  const filteredVenues = data.filter((venue) => 
+    venue.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
   return (
-    <div className="w-auto grid md:grid-cols-2 gap-6">
-      {data.map((venue, index) => (
-        <div key={venue.id} className="venueContainer">
-          <Link to={`/venue/${venue.id}`}>
-            <img src={venue.media} className="venueImage" alt="Venue" />
-            <div className="venueInfo">
-              <h1 className="mb-1">{venue.name}</h1>
-              <div className="flex gap-1">
-                <p>{venue.price}kr a night |</p>
-                <p>max {venue.maxGuests} guests</p>
+    <div>
+      <div className='flex justify-center'>
+        <input type='text' className='my-4 py-2 px-1 w-1/2' placeholder='Search venues' value={searchTerm} onChange={handleSearch}/>
+      </div>
+      <div className="w-auto grid md:grid-cols-2 gap-6">
+        {filteredVenues.map((venue, index) => (
+          <div key={venue.id} className="venueContainer">
+            <Link to={`/venue/${venue.id}`}>
+              <img src={venue.media} className="venueImage" alt="Venue image" />
+              <div className="venueInfo">
+                <h1 className="mb-1">{venue.name}</h1>
+                <div className="flex gap-1">
+                  <p>{venue.price}kr a night |</p>
+                  <p>max {venue.maxGuests} guests</p>
+                </div>
+                <p>{formattedDates[index]}</p>
               </div>
-              <p>{formattedDates[index]}</p>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
