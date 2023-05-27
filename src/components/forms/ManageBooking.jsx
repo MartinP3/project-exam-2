2872d01180c9f3d622f3e541443c399d1bb480c8
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-05-27 20:15:31 +0200

    updated register component

---
+++ src/components/forms/ManageBooking.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState, useContext } from "react";
+import { BOOKINGS_URL } from "../../utils/ApiUrls";
+import { UserContext } from "../UserContext";
+import { set } from "date-fns";
+
+export function ManageBooking() {
+  const [data, setData] = useState([]);
+  const [deleted, setDeleted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [deleteStatus, setDeleteStatus] = useState("");
+  const { user } = useContext(UserContext);
+  const { token } = user;
+
+  const id = window.location.pathname.substring(
+    window.location.pathname.lastIndexOf("/") + 1
+  );
+  const params = "?_customer=true&_venue=true";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BOOKINGS_URL}/${id}${params}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+        } else {
+          console.log("Failed to fetch the booking data");
+        }
+      } catch (error) {
+        console.log("There was an error fetching the booking data", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const handleDelete = async () => {
+    const response = await fetch(`${BOOKINGS_URL}/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      setDeleted(true);
+      setDeleteStatus("Booking is now deleted");
+      setShowModal(true);
+    } else {
+      setDeleteStatus("Failed to delete booking");
+      setShowModal(true);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    if (deleted) {
+      location.href = "/";
+    }
+  };
+
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-4">Manage booking</h1>
+      {data.venue && (
+        <>
+          <img
+            src={data.venue.media}
+            className="max-w-xs"
+          />
+          <p className="text-2xl mb-1">{data.venue.name}</p>
+        </>
+      )}
+      <h2 className="text-xl">Customer</h2>
+      {data.customer && (
+        <div className="pl-2">
+          <p>{data.customer.name}</p>
+          <p>{data.customer.email}</p>
+        </div>
+      )}
+      <h2 className="text-xl">Duration</h2>
+      <div className="pl-2">
+        <p>
+          {formatDate(data.dateFrom)} &rarr; {formatDate(data.dateTo)}
+        </p>
+      </div>
+      <h2 className="text-xl">Guests</h2>
+      <div className="pl-2">
+        <p>{data.guests}</p>
+      </div>
+    </div>
+  );
+}
--- src/components/forms/Register.jsx
+++ src/components/forms/Register.jsx
@@ -1,59 +1,113 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { REGISTER_URL } from '../../utils/ApiUrls';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { REGISTER_URL } from "../../utils/ApiUrls";
 
 export function RegisterForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = async (data) => {
     try {
       const response = await fetch(REGISTER_URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (response.ok) {
-        console.log('Registration successful');
+        console.log("Registration successful");
+        location.href = "/login";
       } else {
-        console.log('Registration failed :(');
+        console.log("Registration failed :(");
       }
     } catch (error) {
-      console.log('There was an error registering your account', error);
+      console.log("There was an error registering your account", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="bg-neutral-700 py-3 px-5 w-96">
-      <h1 className='mb-2 text-3xl'>Register Form</h1>
-      <h2 className='mb-2 text-md'>* are required</h2>
-      <div className='mb-1'>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="py-3 px-5 w-96"
+    >
+      <h1 className="mb-2 text-3xl">Register Form</h1>
+      <h2 className="mb-2 text-md">* are required</h2>
+      <div className="mb-1">
         <p>Name*</p>
-        <input type='text' className='mb-1 py-1 px-0.5 w-full' {...register("name", { required: "Please enter your full name.", minLength: { value: 3, message: "It must be at least 3 letters long (sorry if your name is only 2 characters long..." } })} placeholder="Billy Bob"/>
-        <p className='text-red-400 -mt-1'>{errors.name?.message}</p>
+        <input
+          type="text"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("name", {
+            required: "Please enter your full name.",
+            minLength: {
+              value: 3,
+              message:
+                "It must be at least 3 letters long (sorry if your name is only 2 characters long...",
+            },
+            pattern: {
+              value: /^[a-zA-Z_]+$/,
+              message: "Only a-z and _ allowed",
+            },
+          })}
+          placeholder="Billy_Bob"
+        />
+        <p className="text-red-400 -mt-1">{errors.name?.message}</p>
       </div>
-      <div className='mb-1'>
+      <div className="mb-1">
         <p>Email*</p>
-        <input type='email' className='mb-1 py-1 px-0.5 w-full' {...register("email", { required: "Please enter a valid email.", pattern: { value: /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/, message: "Please enter a valid email"}})} placeholder="example@email.com"/>
-        <p className='text-red-400 -mt-1'>{errors.email?.message}</p>
+        <input
+          type="email"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("email", {
+            required: "Please enter a valid email.",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@stud\.noroff\.no$/,
+              message: "Please enter stud.noroff.no email.",
+            },
+          })}
+          placeholder="example@email.com"
+        />
+        <p className="text-red-400 -mt-1">{errors.email?.message}</p>
       </div>
-      <div className='mb-1'>
+      <div className="mb-1">
         <p>Password*</p>
-        <input type='password' className='mb-1 py-1 px-0.5 w-full' {...register("password", { required: "Please enter a password.", minLength: { value: 6, message: "Min length should be 6" } })} placeholder="********"/>
-        <p className='text-red-400 -mt-1'>{errors.password?.message}</p>
+        <input
+          type="password"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("password", {
+            required: "Please enter a password.",
+            minLength: { value: 8, message: "Min length should be 8." },
+          })}
+          placeholder="********"
+        />
+        <p className="text-red-400 -mt-1">{errors.password?.message}</p>
       </div>
-      <div className='mb-2'>
+      <div className="mb-2">
         <p>Avatar</p>
-        <input type='url' className='mb-1 py-1 px-0.5 w-full' {...register("url")} placeholder="Avatar url"/>
-        <p className='text-red-400 -mt-1'>{errors.url?.message}</p>
+        <input
+          type="url"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("url")}
+          placeholder="Avatar url"
+        />
+        <p className="text-red-400 -mt-1">{errors.url?.message}</p>
       </div>
-      <div className='mb-2'>
-          <p>Venue Manager</p>
-          <input type="checkbox" {...register("venueManager", {})} />
+      <div className="mb-2">
+        <p>Venue Manager</p>
+        <input
+          type="checkbox"
+          {...register("venueManager", {})}
+        />
       </div>
-      <input type="submit" className='p-3 mt-2 w-full cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500'/>
+      <input
+        type="submit"
+        className="p-3 mt-2 uppercase cursor-pointer w-full text-green-400 shadow-md shadow-green-400 hover:text-green-500 hover:shadow-green-500"
+      />
     </form>
-  )
+  );
 }
