b7a5ae13b322a2f8c6ad93cefd1134ff79207e55
Martin P <70173574+MartinP3@users.noreply.github.com>
2023-05-27 20:14:54 +0200

    added more parts for use to the UseContext, so upon login it'll show appropriate information to the user

--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -1,52 +1,89 @@
-import {  useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { LOGIN_URL } from '../../utils/ApiUrls';
+import { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { LOGIN_URL } from "../../utils/ApiUrls";
+import { UserContext } from "../UserContext";
 
 export function LoginForm() {
-
-  const { register, handleSubmit, formState: {errors} } = useForm();
-  const [accessToken, setAccessToken] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { setUser } = useContext(UserContext);
 
   const onSubmit = async (data) => {
     try {
       const response = await fetch(LOGIN_URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (response.ok) {
-        console.log('Login successful');
-        const data = await response.json();
-        setAccessToken(data.accessToken);
-        localStorage.setItem('accessToken', data.accessToken);
-        console.log(data.accessToken);
-        
+        const responseData = await response.json();
+        const accessToken = responseData.accessToken;
+        const name = responseData.name;
+        const venueManager = responseData.venueManager;
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("userName", name);
+        localStorage.setItem("venueManager", JSON.stringify(venueManager));
+        setUser((prevUser) => ({
+          ...prevUser,
+          loggedIn: true,
+          token: accessToken,
+          name: name,
+          venueManager: venueManager,
+        }));
       } else {
-        console.log('Login failed :(');
+        console.log("Login failed :(");
       }
     } catch (error) {
-      console.log('There was an error logging into your account', error);
+      console.log("There was an error logging into your account", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="bg-neutral-700 py-3 px-5 w-96">
-      <h1 className='mb-2 text-3xl'>Login Form</h1>
-      <h2 className='mb-2 text-md'>* are required</h2>
-      <div className='mb-1'>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="py-3 px-5 w-96"
+    >
+      <h1 className="mb-2 text-3xl">Login Form</h1>
+      <h2 className="mb-2 text-md">* are required</h2>
+      <div className="mb-1">
         <p>Email*</p>
-        <input type="email" className='mb-1 py-1 px-0.5 w-full' {...register("email", { required: "Please enter a valid email.", pattern: { value: /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/, message: "Please enter a valid email"}})} placeholder="example@email.com"/>
-        <p className='text-red-400 -mt-1'>{errors.email?.message}</p>
+        <input
+          type="email"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("email", {
+            required: "Please enter a valid email.",
+            pattern: {
+              value: /[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/,
+              message: "Please enter a valid email",
+            },
+          })}
+          placeholder="example@email.com"
+        />
+        <p className="text-red-400 -mt-1">{errors.email?.message}</p>
       </div>
-      <div className='mb-1'>
+      <div className="mb-1">
         <p>Password*</p>
-        <input type="password" className='mb-1 py-1 px-0.5 w-full' {...register("password", { required: "Please enter your password.", minLength: { value: 6, message: "Min length is 6" } })} placeholder="********"/>
-        <p className='text-red-400 -mt-1'>{errors.password?.message}</p>
+        <input
+          type="password"
+          className="mb-1 py-1 px-0.5 w-full"
+          {...register("password", {
+            required: "Please enter your password.",
+            minLength: { value: 6, message: "Min length is 6" },
+          })}
+          placeholder="********"
+        />
+        <p className="text-red-400 -mt-1">{errors.password?.message}</p>
       </div>
-      <input type="submit" className='p-3 mt-2 w-full cursor-pointer bg-gradient-to-r from-cyan-500 to-teal-500'/>
+      <input
+        type="submit"
+        className="p-3 mt-2 uppercase cursor-pointer w-full text-green-400 shadow-md shadow-green-400 hover:text-green-500 hover:shadow-green-500"
+      />
     </form>
-  )
+  );
 }
